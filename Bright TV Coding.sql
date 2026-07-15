ad38f932f49c37b9a87fb18f4857b58fd2e2b833
lifasiqueenm@gmail.com <lifasiqueenm@gmail.com>
2026-07-15 08:59:23 +0000

    Gender checks
    Cleaning the gender column to remove none and empty spaces

--- Bright TV Coding.sql
+++ Bright TV Coding.sql
@@ -1,8 +1,24 @@
 -- Databricks notebook source
-SELECT *
-FROM sql_fundamentals.brighttv.user_profiles;
-
-SELECT *
-FROM sql_fundamentals.brighttv.viewership;
-
+--Telling databricks to use sql_fundamentals catalog and brighttv schema to shorten the table path
 USE sql_fundamentals.brighttv;
+
+--Running full tables to see what I have
+SELECT *
+FROM user_profiles;
+
+SELECT *
+FROM viewership;
+
+----Gender checks---
+SELECT DISTINCT Gender
+FROM user_profiles;
+
+--cleaning up gender
+SELECT DISTINCT 
+    CASE 
+        WHEN Gender = ' ' THEN 'unknown' --replaces empty space with unknown
+        WHEN Gender = 'None' THEN 'unknown' --replaces none with unknown
+        WHEN Gender IS NULL THEN 'unknown' --replaces null with unknown
+    ELSE Gender --returns the gender as it is on the data
+    END AS gender_clean --my new gender column name 
+FROM user_profiles;
