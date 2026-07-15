75ef05687ea9c60d454c19a3f570ec306b4fe716
lifasiqueenm@gmail.com <lifasiqueenm@gmail.com>
2026-07-15 09:12:22 +0000

    Race checks and cleaned my data

--- Bright TV Coding.sql
+++ Bright TV Coding.sql
@@ -22,3 +22,16 @@ SELECT DISTINCT
     ELSE Gender --returns the gender as it is on the data
     END AS gender_clean --my new gender column name 
 FROM user_profiles;
+
+---Race Checks---
+SELECT DISTINCT Race
+FROM user_profiles;
+
+SELECT DISTINCT 
+    CASE 
+        WHEN Race = 'other' THEN 'unknown' 
+        WHEN Race = ' ' THEN 'unknown' 
+        WHEN Race = 'None' THEN 'unknown'
+    ELSE Race 
+    END AS race_clean
+FROM user_profiles;
